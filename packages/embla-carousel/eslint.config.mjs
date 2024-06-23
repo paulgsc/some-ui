50e977569d9ffe564cadf72741324316063a8ad7
Paul Gathondu <aulgondu@gmail.com>
2024-06-23 01:24:04 -0700

    refactor: update package eslint

--- packages/embla-carousel/eslint.config.mjs
+++ packages/embla-carousel/eslint.config.mjs
@@ -1,7 +1,5 @@
 import { default as baseConfig } from '../../eslint.config.mjs' // Import base configuration from JSON
 
-import tsParser from '@typescript-eslint/parser' // TypeScript parser
-
 export default [
   ...baseConfig // Spread base configuration
 ]
