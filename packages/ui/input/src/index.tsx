723cc02a085a52d5f9a7c6b064f8cc7eed392a35
Paul Gathondu <aulgondu@gmail.com>
2024-08-11 19:50:53 -0700

    test: test eslint

--- packages/ui/input/src/index.tsx
+++ packages/ui/input/src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
 import cn from "@some-ui/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+//test
+export type InputProps = {} & React.InputHTMLAttributes<HTMLInputElement>
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
