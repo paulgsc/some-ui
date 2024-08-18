f987a0305c28ba8bfc67112c1e8a9209138ad14f
Paul Gathondu <aulgondu@gmail.com>
2024-08-18 14:27:01 -0700

    fix: add alias resolve to root for storybook compatability

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import path from "path"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@pivot-search": path.resolve(__dirname, "./packages/pivot-search/src"),
+      // Add more aliases for other packages as needed
+    },
+  },
+  // This allows Vite to resolve packages within the monorepo
+  server: {
+    fs: {
+      allow: [".."],
+    },
+  },
 })
