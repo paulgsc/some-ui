0393962f443cd8735e8296fc2137c056bc192f7c
Paul Gathondu <aulgondu@gmail.com>
2024-08-18 14:28:05 -0700

    fix: add vite alias resolve

--- packages/pivot-search/vite.config.ts
+++ packages/pivot-search/vite.config.ts
@@ -1,7 +1,12 @@
+import path from "path"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@pivot-search": path.resolve(__dirname, "./src"),
+    },
+  },
 })
