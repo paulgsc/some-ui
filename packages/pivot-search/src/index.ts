d7876c7d3e9e4783277ae3bd8a3785e87e24c4d0
Paul Gathondu <aulgondu@gmail.com>
2024-08-24 19:57:27 -0700

    fix: use index.ts for root

---
+++ packages/pivot-search/src/index.ts
@@ -0,0 +1,8 @@
+export { Axis, Draggable, DraggableOverlay } from "@pivot-search/components"
+export { Action } from "@pivot-search/components"
+export { Handle } from "@pivot-search/components"
+export { Grid } from "@pivot-search/components"
+export { OverflowWrapper } from "@pivot-search/components"
+export { Wrapper } from "@pivot-search/components"
+export { Droppable } from "@pivot-search/components"
+export { GridContainer } from "@pivot-search/components"
