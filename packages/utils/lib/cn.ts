b25e231f64fe47277893f35d55433ff80d186ae0
Paul Gathondu <aulgondu@gmail.com>
2024-08-24 21:01:27 -0700

    refactor: remove simple sort eslint

--- packages/utils/lib/cn.ts
+++ packages/utils/lib/cn.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export default function cn(...inputs: Array<ClassValue>) {
+export default function cn(...inputs: Array<ClassValue>): string {
   return twMerge(clsx(inputs))
 }
