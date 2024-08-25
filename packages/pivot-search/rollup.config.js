0d8c494ac5c10d33300b4282700a362b5c4357db
Paul Gathondu <aulgondu@gmail.com>
2024-08-24 19:50:13 -0700

    feat: use shared rollup config dependency for workspace bundling

--- packages/pivot-search/rollup.config.js
+++ packages/pivot-search/rollup.config.js
@@ -1,75 +1,10 @@
-import {
-  babel,
-  CONFIG_BABEL,
-  CONFIG_EXTERNAL_MODULE_SUPPRESS,
-  CONFIG_EXTERNAL_MODULES,
-  CONFIG_GLOBALS,
-  CONFIG_TYPESCRIPT,
-  createBuildPath,
-  createNodeNextSupport,
-  FOLDERS,
-  kebabToPascalCase,
-  resolve,
-  terser,
-  typescript,
-} from "../../rollup.config"
+import path from "path"
+import { createRollupConfig } from "@some-ui/rollup-config"
+
 import packageJson from "./package.json"
 
-const CONFIG_GLOBALS_MODULE = {
-  ...CONFIG_GLOBALS,
-  react: "React",
+const CONFIG_TYPESCRIPT = {
+  tsconfig: path.join(__dirname, "tsconfig.json"),
 }
 
-const CONFIG_GLOBALS_UMD = {
-  react: "React",
-}
-
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: createBuildPath(packageJson, FOLDERS.CJS),
-        format: FOLDERS.CJS,
-        globals: CONFIG_GLOBALS_MODULE,
-        strict: true,
-        sourcemap: true,
-        exports: "auto",
-        plugins: resolve(CONFIG_EXTERNAL_MODULES),
-      },
-      {
-        file: createBuildPath(packageJson, FOLDERS.ESM),
-        format: FOLDERS.ESM,
-        globals: CONFIG_GLOBALS_MODULE,
-        strict: true,
-        sourcemap: true,
-        plugins: resolve(CONFIG_EXTERNAL_MODULES),
-      },
-    ],
-    onwarn: CONFIG_EXTERNAL_MODULE_SUPPRESS,
-    plugins: [resolve(), typescript(CONFIG_TYPESCRIPT), babel(CONFIG_BABEL)],
-    external: Object.keys(CONFIG_GLOBALS_MODULE),
-  },
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: createBuildPath(packageJson, FOLDERS.UMD),
-        format: FOLDERS.UMD,
-        globals: CONFIG_GLOBALS_UMD,
-        strict: true,
-        sourcemap: false,
-        name: kebabToPascalCase(packageJson.name),
-        plugins: [resolve(), terser()],
-      },
-    ],
-    onwarn: CONFIG_EXTERNAL_MODULE_SUPPRESS,
-    plugins: [
-      resolve(),
-      typescript(CONFIG_TYPESCRIPT),
-      babel(CONFIG_BABEL),
-      createNodeNextSupport(),
-    ],
-    external: Object.keys(CONFIG_GLOBALS_UMD),
-  },
-]
+export default createRollupConfig(packageJson, CONFIG_TYPESCRIPT)
