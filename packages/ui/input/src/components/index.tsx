d42db63603bc3f219f0514efb024cc7a4bd73f07
Paul Gathondu <aulgondu@gmail.com>
2024-08-24 19:58:32 -0700

    feat: add component demo to test build

---
+++ packages/ui/input/src/components/index.tsx
@@ -0,0 +1,5 @@
+const Input = (): JSX.Element => {
+  return <h1 className="text-xl text-red-500"> me is input</h1>
+}
+
+export default Input
---
+++ packages/ui/resume/src/components/index.tsx
@@ -0,0 +1,5 @@
+const Resume = (): JSX.Element => {
+  return <h1 className="text-xl text-red-500"> me is Resume</h1>
+}
+
+export default Resume
