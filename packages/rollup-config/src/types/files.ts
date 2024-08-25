86d90f572e01421c67d90b61f4573fa8fb806a94
Paul Gathondu <aulgondu@gmail.com>
2024-08-24 19:47:28 -0700

    refactor: switch rollup from root to workspace

---
+++ packages/rollup-config/src/types/files.ts
@@ -0,0 +1,54 @@
+export type IPackageJsonDependencyTypes =
+  | "dependencies"
+  | "devDependencies"
+  | "peerDependencies"
+  | "optionalDependencies"
+
+export type IPackageJsonAddress = {
+  email?: string
+  url?: string
+}
+
+export type IPackageJsonPerson = {
+  name: string
+} & IPackageJsonAddress
+
+export type PackageJsonTypes = {
+  name: string
+  version: string
+  description?: string
+  keywords?: string
+  homepage?: string
+  bugs?: IPackageJsonAddress
+  license?: string
+  author?: string | IPackageJsonPerson
+  contributors?: string[] | IPackageJsonPerson[]
+  files?: string[]
+  main?: string
+  browser?: string
+  bin?: Record<string, string>
+  man?: string
+  directories?: {
+    lib?: string
+    bin?: string
+    man?: string
+    doc?: string
+    example?: string
+    test?: string
+  }
+  repository?: {
+    type?: "git"
+    url?: string
+    directory?: string
+  }
+  scripts?: Record<string, string>
+  config?: Record<string, string>
+  dependencies?: Record<string, string>
+  devDependencies?: Record<string, string>
+  peerDependencies?: Record<string, string>
+  optionalDependencies?: Record<string, string>
+  bundledDependencies?: string[]
+  engines?: Record<string, string>
+  os?: string[]
+  cpu?: string[]
+}
