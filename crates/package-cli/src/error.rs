d5799b5b7d9d7ea0c6d70c081932e69165d0c20d
Paul Gathondu <aulgondu@gmail.com>
2024-08-26 13:32:06 -0700

    fix: add missing deps, fix error handling

--- crates/package-cli/src/error.rs
+++ crates/package-cli/src/error.rs
@@ -4,10 +4,19 @@ use thiserror::Error;
 pub enum PackageError {
     #[error("Failed to create directory: {0}")]
     DirectoryCreationError(String),
+
     #[error("Failed to copy file to {0}")]
     FileCopyError(String),
+
     #[error("Failed to run pnpm install")]
     CommandError,
+    
+    #[error("I/O error: {0}")]
+    IoError(#[from] std::io::Error),
+
+    #[error("JSON error: {0}")]
+    JsonError(#[from] serde_json::Error),
+
 }
 
 pub type Result<T> = std::result::Result<T, PackageError>;
