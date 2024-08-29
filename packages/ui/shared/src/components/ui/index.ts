2526b5bd403717f7e6b82cb84b92e4e069ec5cbb
Paul Gathondu <aulgondu@gmail.com>
2024-08-28 22:09:57 -0700

    feat: export necessary modules

---
+++ packages/ui/shared/src/components/ui/index.ts
@@ -0,0 +1,27 @@
+export { Alert, AlertTitle, AlertDescription } from "./alert"
+export { AspectRatio } from "./aspect-ratio"
+export { Button, buttonVariants } from "./button"
+
+export {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+} from "./dropdown-menu"
+
+export {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+} from "./resizable"
