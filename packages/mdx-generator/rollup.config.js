4f2d25f11fdf56c99dcce1638c9e4d5234e90096
Paul Gathondu <aulgondu@gmail.com>
2024-08-29 21:56:14 -0700

    fix: don't rebundle pacakges, declare external deps

--- packages/mdx-generator/rollup.config.js
+++ packages/mdx-generator/rollup.config.js
@@ -7,6 +7,14 @@ const CONFIG_TYPESCRIPT = {
   tsconfig: path.join(__dirname, "tsconfig.json"),
 }
 
-const external = ["lucide-react", "@radix-ui/react-dropdown-menu"]
+const external = [
+  "lucide-react",
+  "@radix-ui/react-dropdown-menu",
+  "next",
+  "next-contentlayer2",
+  "next-contentlayer2/hooks",
+  "some-ui-shared",
+  "some-ui-utils",
+]
 
 export default createRollupConfig(packageJson, CONFIG_TYPESCRIPT, external)
