8230382923723eda71cc7b8809423b20be97b60c
Paul Gathondu <aulgondu@gmail.com>
2024-09-01 13:10:09 -0700

    feat: add nuqs hook

---
+++ packages/utils/src/lib/hooks/use-string-query-state/use-string-query-state.ts
@@ -0,0 +1,29 @@
+import { parseAsStringLiteral, useQueryState } from "nuqs"
+
+// Replace with the actual import path
+
+type QueryStateOptions = {
+  defaultValue: string
+  validValues: Array<string>
+  options?: {
+    clearOnDefault?: boolean
+  }
+}
+
+type QueryStateReturnType = ReturnType<typeof useQueryState>
+
+export function useGeneralizedQueryState(
+  key: string,
+  config: QueryStateOptions
+): QueryStateReturnType {
+  const { defaultValue, validValues, options } = config
+
+  const [value, setValue] = useQueryState(
+    key,
+    parseAsStringLiteral(validValues)
+      .withDefault(defaultValue)
+      .withOptions({ ...options })
+  )
+
+  return [value, setValue] as const
+}
