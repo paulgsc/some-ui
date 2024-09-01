54e10442046884f05097c7e964bd6ccf25afe687
Paul Gathondu <aulgondu@gmail.com>
2024-09-01 12:22:17 -0700

    refactor: export select ui

--- packages/ui/shared/src/components/ui/index.ts
+++ packages/ui/shared/src/components/ui/index.ts
@@ -2,32 +2,14 @@ export { Alert, AlertTitle, AlertDescription } from "./alert"
 export { AspectRatio } from "./aspect-ratio"
 export { Button, buttonVariants, ButtonProps } from "./button"
 
-export {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuCheckboxItem,
-  DropdownMenuRadioItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuGroup,
-  DropdownMenuPortal,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuRadioGroup,
-} from "./dropdown-menu"
+export * from "./dropdown-menu"
 
-export {
-  ResizablePanelGroup,
-  ResizablePanel,
-  ResizableHandle,
-} from "./resizable"
+export * from "./resizable"
 
 export {
   Collapsible,
   CollapsibleTrigger,
   CollapsibleContent,
 } from "./collapsible"
+
+export * from "./select"
