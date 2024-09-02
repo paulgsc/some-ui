18fccac750631b1c6efe6704fb6f407854342230
Paul Gathondu <aulgondu@gmail.com>
2024-09-02 14:10:11 -0700

    refactor: use generics

--- packages/utils/src/lib/hooks/use-string-query-state/use-string-query-state.ts
+++ packages/utils/src/lib/hooks/use-string-query-state/use-string-query-state.ts
@@ -2,20 +2,26 @@ import { parseAsStringLiteral, useQueryState } from "nuqs"
 
 // Replace with the actual import path
 
-export type QueryStateOptions = {
-  defaultValue: string
-  menu: Array<string>
+export type QueryStateOptions<T extends string> = {
+  defaultValue: T
+  menu: Array<T>
   options?: {
     clearOnDefault?: boolean
   }
 }
 
-type QueryStateReturnType = ReturnType<typeof useQueryState>
+type UseQueryStateReturnType<T> = [
+  value: T | null,
+  setValue: (
+    value: T | ((old: T | null) => T | null) | null,
+    options?: { clearOnDefault?: boolean }
+  ) => Promise<URLSearchParams>,
+]
 
-export function useStringQueryState(
+export function useStringQueryState<T extends string>(
   key: string,
-  config: QueryStateOptions
-): QueryStateReturnType {
+  config: QueryStateOptions<T>
+): UseQueryStateReturnType<T> {
   const { defaultValue, menu, options } = config
 
   const [value, setValue] = useQueryState(
