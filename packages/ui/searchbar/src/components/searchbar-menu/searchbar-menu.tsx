a0094b5f3f50db3d3595990f2b2b867f24bef28d
Paul Gathondu <aulgondu@gmail.com>
2024-09-02 12:09:38 -0700

    refactor: use select menu

--- packages/ui/searchbar/src/components/searchbar-menu/searchbar-menu.tsx
+++ packages/ui/searchbar/src/components/searchbar-menu/searchbar-menu.tsx
@@ -1,15 +1,9 @@
 import type { FC } from "react"
 import { defaultConfig } from "@searchbar/components/data"
 import { useSearchBarState } from "@searchbar/hooks"
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "some-ui-shared"
 import type { QueryStateOptions } from "some-ui-utils"
+
+import SelectMenu from "./select-menu"
 
 type SearchBarContextMenuProps = {
   config?: QueryStateOptions
@@ -24,29 +18,11 @@ const SearchBarContextMenu: FC<SearchBarContextMenuProps> = ({
     param: param,
   })
   return (
-    <Select
-      onValueChange={(value) => {
-        void setContext(value)
-      }}
-      defaultValue={context}
-    >
-      <SelectTrigger className="h-full max-w-32 rounded-none rounded-r-full bg-primary-foreground capitalize outline-none ring-0 ring-offset-0 focus:outline-none focus:ring-0 focus:ring-offset-0">
-        <SelectValue placeholder={"foo"} />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup defaultValue={context}>
-          {menuItems.map((menu) => (
-            <SelectItem
-              key={menu}
-              value={menu}
-              className="shrink-0 text-sm tracking-tight"
-            >
-              {menu}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <SelectMenu
+      context={context}
+      menuItems={menuItems}
+      setContext={setContext}
+    />
   )
 }
 
