ac6be093a7c4f14ed8f04017d3de93f4a1e06507
Paul Gathondu <aulgondu@gmail.com>
2024-09-02 11:06:48 -0700

    refactor: add param prop

--- packages/ui/searchbar/src/hooks/use-searchbar-state.tsx
+++ packages/ui/searchbar/src/hooks/use-searchbar-state.tsx
@@ -1,19 +1,23 @@
 import { searchContextSchema } from "@searchbar/types/searchbar"
 import { useStringQueryState, type QueryStateOptions } from "some-ui-utils"
 
-// Assuming you have a schema
 type QueryStateReturnType = ReturnType<typeof useStringQueryState>
 
 type UseSearchBarStateReturn = {
-  context: string // Assuming the context is a string, adjust if it's a different type
+  context: string
   setContext: QueryStateReturnType[1]
-  menuItems: Array<string> // Assuming the menu items are strings
+  menuItems: Array<string>
 }
 
-export const useSearchBarState = (
+type UseSearchBarStateProps<T extends string> = {
   config: QueryStateOptions
-): UseSearchBarStateReturn => {
-  const [urlContext, setUrlContext] = useStringQueryState("t", config)
+  param: T
+}
+export const useSearchBarState = <T extends string>({
+  config,
+  param,
+}: UseSearchBarStateProps<T>): UseSearchBarStateReturn => {
+  const [urlContext, setUrlContext] = useStringQueryState(param, config)
   const contextFromParam = searchContextSchema.safeParse(urlContext).data
   const defaultContext = contextFromParam ?? config.defaultValue
 
