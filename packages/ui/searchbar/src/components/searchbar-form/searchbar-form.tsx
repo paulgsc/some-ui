eaa90b093f599296adb62f273939a903863148ff
Paul Gathondu <aulgondu@gmail.com>
2024-09-02 15:46:18 -0700

    feat: setup form

--- packages/ui/searchbar/src/components/searchbar-form/searchbar-form.tsx
+++ packages/ui/searchbar/src/components/searchbar-form/searchbar-form.tsx
@@ -1,22 +1,20 @@
-import { Button } from "@/components/ui/button"
-import { Icons } from "@/components/icons"
+import { SearchBarContextMenu } from "@searchbar/components/searchbar-menu"
+import { SearchBarToggle } from "@searchbar/components/searchbar-toggle"
+import { Button, SvgIcons } from "some-ui-shared"
 
-import { CloseSearchBarBtn } from "./close-searchbar"
-import SearchBarContextMenu from "./search-context-menu"
-
-const SearchBarForm = () => {
+const SearchBarForm = (): JSX.Element => {
   return (
     <form
       role="search"
-      className="bg-primary-foreground hidden h-14 flex-1 items-center rounded-full lg:flex"
+      className="hidden h-14 flex-1 items-center rounded-full bg-primary-foreground lg:flex"
     >
       <section className="relative size-full">
-        <CloseSearchBarBtn role="status">
-          <Icons.cross className="text-muted-foreground size-3 shrink-0 transition-transform hover:scale-105 hover:text-pink-500" />
-        </CloseSearchBarBtn>
+        <SearchBarToggle param="foo" role="status">
+          <SvgIcons.cross className="size-3 shrink-0 text-muted-foreground transition-transform hover:scale-105 hover:text-pink-500" />
+        </SearchBarToggle>
         <input
           type="text"
-          className="bg-card size-full rounded-l-full p-2.5 ps-10 text-sm text-gray-900 outline-none focus:ring-1 dark:text-white"
+          className="size-full rounded-l-full bg-card p-2.5 ps-10 text-sm text-gray-900 outline-none focus:ring-1 dark:text-white"
           placeholder="Search"
           required
         />
@@ -25,10 +23,10 @@ const SearchBarForm = () => {
           variant={"ghost"}
           className="absolute inset-y-0 end-0 flex size-fit h-full items-center rounded-none border-none p-0 px-6"
         >
-          <Icons.magnifyGlass className="size-4" />
+          <SvgIcons.magnifyGlass className="size-4" />
         </Button>
       </section>
-      <SearchBarContextMenu />
+      <SearchBarContextMenu param="foo" />
     </form>
   )
 }
