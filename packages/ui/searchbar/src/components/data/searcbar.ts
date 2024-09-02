66ef8a3d85e4b35b15a82b30411da5272768e7b6
Paul Gathondu <aulgondu@gmail.com>
2024-09-02 14:09:27 -0700

    feat: ensure that usequerystate takes a generic

--- packages/ui/searchbar/src/components/data/searcbar.ts
+++ packages/ui/searchbar/src/components/data/searcbar.ts
@@ -1,14 +1,33 @@
-import type { SearchContextMenu } from "@searchbar/types/searchbar"
+import type {
+  SearchBarRenderType,
+  SearchContext,
+  SearchContextMenu,
+  SearchbarToggleContextMenu,
+} from "@searchbar/types/searchbar"
 import type { QueryStateOptions } from "some-ui-utils"
 
-const SearchMenu: SearchContextMenu = {
+const searchMenu: SearchContextMenu = {
   defaultValue: "navigation",
   menu: ["blog", "navigation", "events", "updates"],
 }
 
-export const defaultConfig: QueryStateOptions = {
-  ...SearchMenu,
-  options: {
-    clearOnDefault: true,
-  },
+export const defaultSearchContextMenuConfig: QueryStateOptions<SearchContext> =
+  {
+    ...searchMenu,
+    options: {
+      clearOnDefault: true,
+    },
+  }
+
+export const searchbarToggle: SearchbarToggleContextMenu = {
+  defaultValue: "fragment",
+  menu: ["fragment", "searchbar"],
 }
+
+export const defaultSearchToggleContextConfig: QueryStateOptions<SearchBarRenderType> =
+  {
+    ...searchbarToggle,
+    options: {
+      clearOnDefault: true,
+    },
+  }
