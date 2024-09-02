f41c908c570b3ad3501236f0f758f3ae8020497e
Paul Gathondu <aulgondu@gmail.com>
2024-09-02 11:00:11 -0700

    fix: typecheck

--- packages/ui/searchbar/src/hooks/use-searchbar-state.tsx
+++ packages/ui/searchbar/src/hooks/use-searchbar-state.tsx
@@ -1,12 +1,21 @@
-import { searchContextSchema } from "@searchbar/validation"
-import { QueryStateOptions, useStringQueryState } from "some-ui-utils"
+import { searchContextSchema } from "@searchbar/types/searchbar"
+import { useStringQueryState, type QueryStateOptions } from "some-ui-utils"
 
 // Assuming you have a schema
+type QueryStateReturnType = ReturnType<typeof useStringQueryState>
 
-export const useSearchBarState = (config: QueryStateOptions) => {
+type UseSearchBarStateReturn = {
+  context: string // Assuming the context is a string, adjust if it's a different type
+  setContext: QueryStateReturnType[1]
+  menuItems: Array<string> // Assuming the menu items are strings
+}
+
+export const useSearchBarState = (
+  config: QueryStateOptions
+): UseSearchBarStateReturn => {
   const [urlContext, setUrlContext] = useStringQueryState("t", config)
   const contextFromParam = searchContextSchema.safeParse(urlContext).data
-  const defaultContext = contextFromParam ?? config.default
+  const defaultContext = contextFromParam ?? config.defaultValue
 
   return {
     context: defaultContext,
