03e7483333ea89bb0afc1d08e2db4ebaf824c550
Paul Gathondu <aulgondu@gmail.com>
2024-09-02 11:59:49 -0700

    refactor: separate select menu logic

---
+++ packages/ui/searchbar/src/components/searchbar-menu/select-menu/select-menu.tsx
@@ -0,0 +1,46 @@
+import type { FC } from "react"
+import type { UseSearchBarStateReturn } from "@searchbar/types/searchbar"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "some-ui-shared"
+
+type SelectMenuProps = UseSearchBarStateReturn
+
+const SelectMenu: FC<SelectMenuProps> = ({
+  context,
+  setContext,
+  menuItems,
+}) => {
+  return (
+    <Select
+      onValueChange={(value) => {
+        void setContext(value)
+      }}
+      defaultValue={context}
+    >
+      <SelectTrigger className="h-full max-w-32 rounded-none rounded-r-full bg-primary-foreground capitalize outline-none ring-0 ring-offset-0 focus:outline-none focus:ring-0 focus:ring-offset-0">
+        <SelectValue placeholder={"foo"} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup defaultValue={context}>
+          {menuItems.map((menu) => (
+            <SelectItem
+              key={menu}
+              value={menu}
+              className="shrink-0 text-sm tracking-tight"
+            >
+              {menu}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
+
+export default SelectMenu
