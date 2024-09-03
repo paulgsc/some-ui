b0f1e4d84b6b77cb0873c84f5903dd5542fb15d0
Paul Gathondu <aulgondu@gmail.com>
2024-09-02 20:00:41 -0700

    fix: update external deps in bundler

--- packages/ui/searchbar/rollup.config.js
+++ packages/ui/searchbar/rollup.config.js
@@ -7,6 +7,6 @@ const CONFIG_TYPESCRIPT = {
   tsconfig: path.join(__dirname, "tsconfig.json"),
 }
 
-const external = ["nuqs"]
+const external = ["nuqs", "react", "react-dom", "@radix-ui/react-select"]
 
 export default createRollupConfig(packageJson, CONFIG_TYPESCRIPT, external)
