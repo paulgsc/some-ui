c99e855d68503ee0bb4b8fb34cecea704d8da30f
Paul Gathondu <aulgondu@gmail.com>
2024-11-15 14:48:15 -0800

    chore: update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,29 +9,32 @@ import type { StorybookConfig } from "@storybook/react-vite"
 const config: StorybookConfig = {
   stories: ["../packages/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   logLevel: "error",
+
   core: {
     disableTelemetry: true,
     disableWhatsNewNotifications: true,
-    builder: {
-      name: "@storybook/builder-vite",
-      options: {
+  },
+
+  addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    "@chromatic-com/storybook",
+  ],
+
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {
+      builder: {
         launchOptions: {
           open: false,
         },
       },
     },
   },
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
+
   viteFinal: (config) => {
     config.define = {
       ...config.define,
@@ -41,5 +44,15 @@ const config: StorybookConfig = {
     }
     return config
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
 }
 export default config
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")))
+}
