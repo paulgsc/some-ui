0c8d53e62860900d7c5fcca4345db9793887e514
Paul Gathondu <aulgondu@gmail.com>
2024-12-23 12:15:15 -0800

    fix: lint

--- packages/ui/shared/src/components/ui/resizable.tsx
+++ packages/ui/shared/src/components/ui/resizable.tsx
@@ -8,7 +8,9 @@ import { SvgIcons } from "../icons"
 const ResizablePanelGroup = ({
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>): JSX.Element => (
+}: React.ComponentProps<
+  typeof ResizablePrimitive.PanelGroup
+>): React.JSX.Element => (
   <ResizablePrimitive.PanelGroup
     className={cn(
       "flex size-full data-[panel-group-direction=vertical]:flex-col",
@@ -26,7 +28,7 @@ const ResizableHandle = ({
   ...props
 }: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
   withHandle?: boolean
-}): JSX.Element => (
+}): React.JSX.Element => (
   <ResizablePrimitive.PanelResizeHandle
     className={cn(
       "relative flex items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
