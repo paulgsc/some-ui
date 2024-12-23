1c579e34170cd54ebe83bb02f024892d93639fa9
Paul Gathondu <aulgondu@gmail.com>
2024-12-23 12:12:04 -0800

    feat: add new tailwind classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,11 +66,22 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        flicker: {
+          "0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%": {
+            opacity: "1",
+          },
+          "20%, 21.999%, 63%, 63.999%, 65%, 69.999%": {
+            opacity: "0.4",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "caret-blink": "caret-blink 1.25s ease-out infinite",
+        "pulse-slow": "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        flicker: "flicker 2s linear infinite",
+        "flicker-fast": "flicker 1.5s linear infinite",
       },
       "caret-blink": {
         "0%,70%,100%": { opacity: "1" },
