4c5d65cd593d10981a013a9aee29bbbaab81f697
Paul Gathondu <aulgondu@gmail.com>
2024-12-26 22:26:20 -0800

    feat(wip): update styles and config for emoji ui

--- packages/ui/emoji-animations/src/hooks/use-battery-state.ts
+++ packages/ui/emoji-animations/src/hooks/use-battery-state.ts
@@ -5,13 +5,14 @@ export const useBatteryState = (initialCharge = 100, dischargeRate = 1) => {
   const [isCharging, setIsCharging] = useState(false)
 
   useEffect(() => {
+    const intervalTime = (100 / 60) * 60000
     const interval = setInterval(() => {
       if (!isCharging && charge > 0) {
         setCharge((prevCharge) => Math.max(prevCharge - dischargeRate, 0))
       } else if (isCharging && charge < 100) {
         setCharge((prevCharge) => Math.min(prevCharge + dischargeRate, 100))
       }
-    }, 100)
+    }, intervalTime)
 
     return (): void => {
       clearInterval(interval)
