c4f97250c58cc733a96910acfac05a0f773c9aa3
Paul Gathondu <aulgondu@gmail.com>
2024-12-28 18:58:35 -0800

    feat(wip): slideshow ui card struggling to do something!

--- packages/ui/slideshow/src/components/sidebar-carousel/index.tsx
+++ packages/ui/slideshow/src/components/sidebar-carousel/index.tsx
@@ -1,38 +1,76 @@
-import {
-  Card,
-  CardContent,
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "some-ui-shared"
+import { useEffect, useRef, useState } from "react"
+import type { FC } from "react"
+import { Card, CardContent } from "some-ui-shared"
+import { cn } from "some-ui-utils"
 
-const SidebarCarousel = () => {
+type DummyData = {
+  id: number
+  title: string
+  content: string
+}
+
+const generateDummyData = (start: number, end: number): Array<DummyData> =>
+  Array.from({ length: end - start }, (_, i) => ({
+    id: start + 1 + i,
+    title: `Item ${start + i}`,
+    content: `This is the content for ${start + i}`,
+  }))
+
+type SideBarCarouselProps = {
+  className?: string
+  start?: number
+  end?: number
+}
+const SidebarCarousel: FC<SideBarCarouselProps> = ({
+  start = 0,
+  end = 3,
+  className,
+}): React.JSX.Element => {
+  const [items, setItems] = useState<Array<DummyData>>(
+    generateDummyData(start, end)
+  )
+  const observerTarget = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (!observerTarget.current) return
+
+    const fetchMoreItems = (): void => {
+      const lastId = items[items.length - 1].id
+      const pgCnt = 3
+      const newItems = generateDummyData(lastId + 1, lastId + pgCnt)
+      setItems((prevItems) => [...prevItems, ...newItems])
+    }
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          fetchMoreItems()
+        }
+      },
+      { threshold: 1.0 }
+    )
+
+    const currentTarget = observerTarget.current
+    observer.observe(currentTarget)
+
+    return (): void => {
+      observer.unobserve(currentTarget)
+    }
+  }, [items])
+
   return (
-    <Carousel
-      opts={{
-        align: "start",
-      }}
-      orientation="vertical"
-      className="w-full max-w-xs"
-    >
-      <CarouselContent className="-mt-1 h-[200px]">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="pt-1 md:basis-1/2">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <aside className={cn("", className)}>
+      {items.map((item) => (
+        <Card key={item.id} className="h-52 w-full rounded-md">
+          <CardContent className="">
+            <h3 className="text-lg font-semibold tracking-tight">
+              {item.title}
+            </h3>
+          </CardContent>
+        </Card>
+      ))}
+
+      <div ref={observerTarget} />
+    </aside>
   )
 }
 
