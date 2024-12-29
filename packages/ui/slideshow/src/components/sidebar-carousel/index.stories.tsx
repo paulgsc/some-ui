016b562d25cfc93328b735398f00767b5f92ccd2
Paul Gathondu <aulgondu@gmail.com>
2024-12-28 18:58:35 -0800

    feat(wip): slideshow ui card struggling to do something!

--- packages/ui/slideshow/src/components/sidebar-carousel/index.stories.tsx
+++ packages/ui/slideshow/src/components/sidebar-carousel/index.stories.tsx
@@ -5,7 +5,19 @@ import SidebarCarousel from "."
 type Story = StoryObj<typeof SidebarCarousel>
 type Meta = MetaObj<typeof SidebarCarousel>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    end: 3,
+    className: "bg-red-500 w-full h-96",
+  },
+  render: (args) => {
+    return (
+      <main className="flex min-h-screen w-full justify-start ">
+        <SidebarCarousel {...args} />
+      </main>
+    )
+  },
+}
 
 export default {
   title: "SlideShow/Carousel/Sidebar",
