e333ff8830eb5d1b65af262179a5ea13aee29176
Paul Gathondu <aulgondu@gmail.com>
2025-01-05 20:41:34 -0800

    chore: force push

--- packages/ui/nfl/src/components/nfl-field/index.tsx
+++ packages/ui/nfl/src/components/nfl-field/index.tsx
@@ -1,4 +1,4 @@
-import type { HTMLAttributes, ReactSVGElement, SVGAttributes } from "react"
+import type { ReactSVGElement, SVGAttributes } from "react"
 import { forwardRef, Fragment, type ComponentPropsWithoutRef } from "react"
 import { cn } from "some-ui-utils"
 
