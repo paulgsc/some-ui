8f8c1a8c6e48377a25503a1fb2961823ceb50d4f
Paul Gathondu <aulgondu@gmail.com>
2025-01-08 21:26:51 -0800

    chore: utils rollup fixes

--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -1,11 +1,16 @@
-import path from "path"
+import path, { resolve } from "path"
 import { createRollupConfig } from "@some-ui/rollup-config"
 
 import packageJson from "./package.json"
 
-const CONFIG_TYPESCRIPT = {
-  tsconfig: path.join(__dirname, "tsconfig.json"),
+const aliasPath = {
+  aliasKey: "@utils",
+  pathVal: resolve(__dirname, "src"),
 }
-const external = ["nuqs", "tailwind-merge", "clsx", "react", "react-dom",  "usehooks-ts"]
+const tsconfig = path.join(__dirname, "tsconfig.build.json")
 
-export default createRollupConfig(packageJson, CONFIG_TYPESCRIPT, external)
+export default createRollupConfig({
+  tsconfig,
+  packageJson,
+  aliasPath: aliasPath,
+})
