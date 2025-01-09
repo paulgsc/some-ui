e3750c0d65cab0c77cab650d03abc8704168d363
Paul Gathondu <aulgondu@gmail.com>
2025-01-08 23:55:59 -0800

    chore: working towards a refactored rollup config

--- packages/ui/shared/rollup.config.js
+++ packages/ui/shared/rollup.config.js
@@ -1,33 +1,16 @@
-import path from "path"
+import path, { resolve } from "path"
 import { createRollupConfig } from "@some-ui/rollup-config"
 
 import packageJson from "./package.json"
 
-const CONFIG_TYPESCRIPT = {
-  tsconfig: path.join(__dirname, "tsconfig.build.json"),
+const aliasPath = {
+  aliasKey: "@shared",
+  pathVal: resolve(__dirname, "src"),
 }
+const tsconfig = path.join(__dirname, "tsconfig.build.json")
 
-const external = [
-  "@radix-ui/react-accordion",
-  "@radix-ui/react-aspect-ratio",
-  "@radix-ui/react-collapsible",
-  "@radix-ui/react-dialog",
-  "@radix-ui/react-slot",
-  "@radix-ui/react-dropdown-menu",
-  "@radix-ui/react-separator",
-  "@radix-ui/react-select",
-  "@radix-ui/react-tabs",
-  "@radix-ui/react-tooltip",
-  "embla-carousel-react",
-  "lucide-react",
-  "react-resizable-panels",
-  "class-variance-authority",
-  "tailwind-merge",
-  "clsx",
-  "react",
-  "react-dom",
-  "recharts",
-  "some-ui-utils",
-]
-
-export default createRollupConfig(packageJson, CONFIG_TYPESCRIPT, external)
+export default createRollupConfig({
+  tsconfig,
+  packageJson,
+  aliasPath: aliasPath,
+})
