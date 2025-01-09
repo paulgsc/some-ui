7baae93ee9e21c1b56d83df102dc5ba54bf65112
Paul Gathondu <aulgondu@gmail.com>
2025-01-09 00:03:28 -0800

    chore: updating packages rollup configs

--- packages/ui/neon-sign/rollup.config.js
+++ packages/ui/neon-sign/rollup.config.js
@@ -1,12 +1,16 @@
-import path from "path"
+import path, { resolve } from "path"
 import { createRollupConfig } from "@some-ui/rollup-config"
 
 import packageJson from "./package.json"
 
-const CONFIG_TYPESCRIPT = {
-  tsconfig: path.join(__dirname, "tsconfig.build.json"),
+const aliasPath = {
+  aliasKey: "@neon-sign",
+  pathVal: resolve(__dirname, "src"),
 }
+const tsconfig = path.join(__dirname, "tsconfig.build.json")
 
-const external = ["some-ui-utils"]
-
-export default createRollupConfig(packageJson, CONFIG_TYPESCRIPT, external)
+export default createRollupConfig({
+  tsconfig,
+  packageJson,
+  aliasPath: aliasPath,
+})
