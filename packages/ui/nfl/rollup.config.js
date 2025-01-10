573833ce1b7593dfd64ca0a4a8c295f25899595f
Paul Gathondu <aulgondu@gmail.com>
2025-01-10 06:21:52 -0800

    chore: update rollup config

--- packages/ui/nfl/rollup.config.js
+++ packages/ui/nfl/rollup.config.js
@@ -1,12 +1,16 @@
-import path from "path"
+import path, { resolve } from "path"
 import { createRollupConfig } from "@some-ui/rollup-config"
 
 import packageJson from "./package.json"
 
-const CONFIG_TYPESCRIPT = {
-  tsconfig: path.join(__dirname, "tsconfig.build.json"),
+const aliasPath = {
+  aliasKey: "@nfl",
+  pathVal: resolve(__dirname, "src"),
 }
+const tsconfig = path.join(__dirname, "tsconfig.build.json")
 
-const external = ["recharts", "react", "react-dom"]
-
-export default createRollupConfig(packageJson, CONFIG_TYPESCRIPT, external)
+export default createRollupConfig({
+  tsconfig,
+  packageJson,
+  aliasPath: aliasPath,
+})
