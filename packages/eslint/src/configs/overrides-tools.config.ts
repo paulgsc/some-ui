760979ed0c336f0fbd2a42b76454cdebc30b7963
Paul Gathondu <aulgondu@gmail.com>
2025-01-10 23:53:11 -0800

    refactor: update to allow for auto discover root file

--- packages/eslint/src/configs/overrides-tools.config.ts
+++ packages/eslint/src/configs/overrides-tools.config.ts
@@ -2,10 +2,7 @@
 import type { ConfigWithExtends } from "typescript-eslint"
 
 export default <ConfigWithExtends>{
-  files: [
-    "**/tools/**/*.{ts,tsx,cts,mts}",
-    "packages/repo-tools/**/*.{ts,tsx,cts,mts}",
-  ],
+  files: ["**/tools/**/*.{ts,tsx,cts,mts}", "repo-tools/**/*.{ts,tsx,cts,mts}"],
   rules: {
     "no-console": "off",
   },
