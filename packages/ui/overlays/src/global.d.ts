1eaa0253203c29ca9f2533bc21f2aa07b3582671
Paul Gathondu <aulgondu@gmail.com>
2025-01-19 00:22:48 -0800

    feat: add logo for youtube overlay

---
+++ packages/ui/overlays/src/global.d.ts
@@ -0,0 +1,15 @@
+// global.d.ts or images.d.ts
+declare module "*.png" {
+  const value: string
+  export default value
+}
+
+declare module "*.jpg" {
+  const value: string
+  export default value
+}
+
+declare module "*.svg" {
+  const content: any
+  export default content
+}
