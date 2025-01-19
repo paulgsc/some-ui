deff7b64644266b1f8a0e38a0cd06ef864aa4a4c
Paul Gathondu <aulgondu@gmail.com>
2025-01-18 23:25:18 -0800

    feat: add logo card

---
+++ packages/ui/overlays/src/components/youtube/logo/index.stories.tsx
@@ -0,0 +1,9 @@
+import Logo from "@overlays/components/youtube/logo"
+import type { Meta as MetaObj, StoryObj } from "@storybook/react"
+
+type Story = StoryObj<typeof Logo>
+type Meta = MetaObj<typeof Logo>
+
+export const Default: Story = {}
+
+export default { title: "Overlays/Youtube/Logo", component: Logo } as Meta
