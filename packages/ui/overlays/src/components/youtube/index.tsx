1ddd66baf71e19a9f4b6f4cbc85d121068891db0
Paul Gathondu <aulgondu@gmail.com>
2025-01-20 02:18:19 -0800

    style: update styles

--- packages/ui/overlays/src/components/youtube/index.tsx
+++ packages/ui/overlays/src/components/youtube/index.tsx
@@ -13,7 +13,7 @@ const YoutubeOverlay = () => {
     [WireframeRegion.MAIN_CONTENT]: <Fragment />,
     [WireframeRegion.FOOTER_LEFT]: <Logo />,
     [WireframeRegion.SIDEBAR_TOP]: (
-      <BoredAnimation className="absolute inset-0 flex flex-col items-center justify-center" />
+      <BoredAnimation className="absolute inset-0 size-full" />
     ),
     [WireframeRegion.SIDEBAR_BOTTOM]: <Fragment />,
     [WireframeRegion.FOOTER_RIGHT]: <Fragment />,
