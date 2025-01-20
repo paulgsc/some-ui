342d15c828a71a8808028ee51fc258f9a8d8a0b3
Paul Gathondu <aulgondu@gmail.com>
2025-01-19 23:20:10 -0800

    refactor: update input shadcn

--- packages/ui/shared/src/components/ui/input.tsx
+++ packages/ui/shared/src/components/ui/input.tsx
@@ -1,15 +1,16 @@
-import { forwardRef, type InputHTMLAttributes } from "react"
+import { forwardRef } from "react"
+import type { ComponentProps, InputHTMLAttributes } from "react"
 import { cn } from "@shared/lib/utils"
 
 export type InputProps = {} & InputHTMLAttributes<HTMLInputElement>
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
+const Input = forwardRef<HTMLInputElement, ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          "border-input bg-background ring-offset-background file:text-foreground placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className
         )}
         ref={ref}
