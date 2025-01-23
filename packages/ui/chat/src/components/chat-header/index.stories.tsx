0dd901e1bc3a02b1f5cbada9065f39e2240edd7a
Paul Gathondu <aulgondu@gmail.com>
2025-01-22 20:48:09 -0800

    feat: add chatbot header

---
+++ packages/ui/chat/src/components/chat-header/index.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta as MetaObj, StoryObj } from "@storybook/react"
+
+import ChatHeader from "."
+
+type Story = StoryObj<typeof ChatHeader>
+type Meta = MetaObj<typeof ChatHeader>
+const characters = [
+  {
+    src: "https://github.com/shadcn.png",
+    alt: "@shadcn",
+    fallback: "CN",
+  },
+  {
+    src: "https://github.com/openai.png",
+    alt: "@openai",
+    fallback: "AI",
+  },
+]
+
+export const Default: Story = {
+  args: {
+    characters,
+  },
+}
+
+export default {
+  title: "UI/Chat/Components/ChatHeader",
+  component: ChatHeader,
+} as Meta
--- packages/ui/shared/src/components/ui/overlay-input/index.tsx
+++ packages/ui/shared/src/components/ui/overlay-input/index.tsx
@@ -3,11 +3,16 @@ import { Input } from "@shared/components/ui"
 import { cn } from "@shared/lib/utils"
 
 type OverlayInputProps = {
+  className?: string
   value: string
   onChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-export const OverlayInput: FC<OverlayInputProps> = ({ value, onChange }) => (
+export const OverlayInput: FC<OverlayInputProps> = ({
+  className,
+  value,
+  onChange,
+}) => (
   <Input
     type="text"
     value={value}
@@ -15,7 +20,8 @@ export const OverlayInput: FC<OverlayInputProps> = ({ value, onChange }) => (
     className={cn(
       "absolute inset-0 size-full border-none bg-transparent text-4xl font-bold tracking-wider text-transparent",
       "focus:border-input focus:ring-2 focus:ring-blue-500 focus:backdrop-blur-lg",
-      "caret-transparent focus:caret-pink-500"
+      "caret-transparent focus:caret-pink-500",
+      className
     )}
   />
 )
