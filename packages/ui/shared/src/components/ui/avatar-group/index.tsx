2cc47d5b7779a042dfeb293ce8d3a080dc6aa4aa
Paul Gathondu <aulgondu@gmail.com>
2025-01-22 20:29:41 -0800

    feat: utilize css var

--- packages/ui/shared/src/components/ui/avatar-group/index.tsx
+++ packages/ui/shared/src/components/ui/avatar-group/index.tsx
@@ -1,4 +1,4 @@
-import type { ComponentProps, FC } from "react"
+import type { ComponentProps, CSSProperties, FC } from "react"
 import { useMemo, useState } from "react"
 import {
   Avatar,
@@ -6,15 +6,22 @@ import {
   AvatarImage,
   Button,
 } from "@shared/components/ui"
+import { cn } from "@shared/lib/utils"
 import { getAcronymFromString } from "some-ui-utils"
 
-type AvatarGroupProps = {
+export type AvatarGroupProps = {
   avatars: Array<ComponentProps<typeof AvatarImage>>
+  className?: string
+  avatarSize?: number
+  avatarSpacing?: number
   limit?: number
   isExpandable?: boolean
 }
 
 const AvatarGroup: FC<AvatarGroupProps> = ({
+  className,
+  avatarSize = 75,
+  avatarSpacing = 30,
   avatars,
   limit = 10,
   isExpandable = true,
@@ -27,9 +34,20 @@ const AvatarGroup: FC<AvatarGroupProps> = ({
   )
 
   return (
-    <div className="flex -space-x-3 rtl:space-x-reverse">
+    <div
+      style={{ "--avatar-spacing": avatarSpacing } as CSSProperties}
+      className={cn(
+        "flex items-center rtl:space-x-reverse",
+        "-space-x-[calc(var(--avatar-spacing)*1px)]",
+        className
+      )}
+    >
       {renderAvatars.map((avatar, index) => (
-        <Avatar key={index} className="size-6">
+        <Avatar
+          style={{ "--avatar-size": avatarSize } as CSSProperties}
+          key={index}
+          className="size-[calc(var(--avatar-size)*1px)]"
+        >
           <AvatarImage className="" src={avatar.src} />
           <AvatarFallback>
             {getAcronymFromString(avatar.alt ?? "")}
