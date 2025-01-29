cf2b2eca3c7bc76b3868d8bbc0f3190a7296a82b
Paul Gathondu <aulgondu@gmail.com>
2025-01-28 22:30:45 -0800

    test: test lint with tsc

--- packages/ui/wireframes/src/components/youtube/resizable-wireframe/index.stories.tsx
+++ packages/ui/wireframes/src/components/youtube/resizable-wireframe/index.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import { ResizableLayout } from "@wireframes/components/youtube/resizable-wireframe"
 
+//comment for test
+
 const meta: Meta<typeof ResizableLayout.Root> = {
   title: "Wireframes/Youtube/ResizableLayout",
   component: ResizableLayout.Root,
