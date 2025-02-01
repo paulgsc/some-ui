e0a76420d00db6be9b8edf143bfa260ad6b152dc
Paul Gathondu <aulgondu@gmail.com>
2025-01-31 21:21:55 -0800

    refactor: add resizable type

--- packages/ui/shared/src/components/ui/resizable.tsx
+++ packages/ui/shared/src/components/ui/resizable.tsx
@@ -4,6 +4,8 @@ import * as ResizablePrimitive from "react-resizable-panels"
 
 import { SvgIcons } from "../icons"
 
+export type ImperativePanelHandle = ResizablePrimitive.ImperativePanelHandle
+
 const ResizablePanelGroup = ({
   className,
   ...props
@@ -28,13 +30,13 @@ const ResizableHandle = ({
 }): React.JSX.Element => (
   <ResizablePrimitive.PanelResizeHandle
     className={cn(
-      "bg-border focus-visible:ring-ring relative flex items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
+      "relative flex items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
     )}
     {...props}
   >
     {withHandle && (
-      <div className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-sm border">
+      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
         <SvgIcons.gripVertical className="size-2.5" />
       </div>
     )}
