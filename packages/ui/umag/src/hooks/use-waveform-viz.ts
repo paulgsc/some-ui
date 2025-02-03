561cadca73a990f089d15ed979f1a79237b65528
Paul Gathondu <aulgondu@gmail.com>
2025-02-03 03:42:19 -0800

    Revert "fix: get umag building"
    This reverts commit 918ae8428885b939c7a6b6f3dfd68f150fe88ef1.

--- packages/ui/umag/src/hooks/use-waveform-viz.ts
+++ packages/ui/umag/src/hooks/use-waveform-viz.ts
@@ -1,29 +1,25 @@
 import { useEffect, useState } from "react"
 
-type WaveFormData = Record<"x" | "y", number>
-
 export function useWaveformVisualization(
   analyser: AnalyserNode | null,
   bars: number,
   height: number,
   width: number
-): Array<WaveFormData> {
+) {
   const barWidth = width / bars
   const midHeight = height / 2
-  const [waveformData, setWaveformData] = useState<Array<WaveFormData>>(
+  const [waveformData, setWaveformData] = useState<Array<Record<"x" | "y", number>>>(
     new Array(bars).fill(height / 2)
   )
 
   useEffect(() => {
     if (!analyser) return
 
-    let animationId: number
-
     const bufferLength = analyser.frequencyBinCount
     const dataArray = new Uint8Array(bufferLength)
 
-    const draw = (): void => {
-      animationId = requestAnimationFrame(draw)
+    const draw = () => {
+      const animationId = requestAnimationFrame(draw)
       analyser.getByteTimeDomainData(dataArray)
 
       const sampledData = Array.from({ length: bars }, (_, i) => {
@@ -40,7 +36,7 @@ export function useWaveformVisualization(
 
     draw()
 
-    return (): void => cancelAnimationFrame(animationId)
+    return () => cancelAnimationFrame(draw)
   }, [analyser, bars, height])
 
   return waveformData
