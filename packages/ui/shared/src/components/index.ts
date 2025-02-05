31ef6a6102c9f193431171b4ff2fac78866b9275
Paul Gathondu <aulgondu@gmail.com>
2025-02-04 20:24:36 -0800

    fix: remove alias from index files

--- packages/ui/shared/src/components/index.ts
+++ packages/ui/shared/src/components/index.ts
@@ -1,2 +1,2 @@
-export * from "@shared/components/ui"
-export * from "@shared/components/icons"
+export * from "./ui"
+export * from "./icons"
--- packages/ui/shared/src/index.ts
+++ packages/ui/shared/src/index.ts
@@ -1,2 +1,2 @@
-export * from "@shared/components"
+export * from "./components"
 //
