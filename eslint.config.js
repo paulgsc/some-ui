2a68026255a91cbab6b76fec1730d250aeedefda
Paul Gathondu <aulgondu@gmail.com>
2025-02-28 14:52:41 -0800

    chore: to do comment

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
-import {maishatuNonStylistic} from "maishatu-eslint-kit"
+/*TODO: update back to recommended once tailwind linter is fixed */
+import { maishatuNonStylistic } from "maishatu-eslint-kit"
 
 export default maishatuNonStylistic
