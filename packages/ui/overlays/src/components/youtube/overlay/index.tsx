e0142f8c55269e1a7193f273c6d83b30f9407847
Paul Gathondu <aulgondu@gmail.com>
2025-03-30 06:19:33 -0700

    feat(wip): data fetch works, ui needs fixing

--- packages/ui/overlays/src/components/youtube/overlay/index.tsx
+++ packages/ui/overlays/src/components/youtube/overlay/index.tsx
@@ -1,12 +1,18 @@
 import { Fragment } from "react"
 import { SplayAnimation, YoutubeMarquee } from "@overlays/components"
 import Logo from "@overlays/components/youtube/logo"
+// import { useGanttChapters } from "@overlays/data/gantt-data"
 import { BoredAnimation } from "some-ui-emoji-animations"
 import { RotatingCube, RotatingNeonSign } from "some-ui-slideshow"
 import type { WireframeContent } from "wireframes"
 import { WireframeRegion, YoutubeWireframe } from "wireframes"
 
 const YoutubeOverlay = (): React.JSX.Element => {
+  // const params = {
+  //   range: "gantt!A1:L20",
+  // }
+  //  const { data: chapters, isLoading, error } = useGanttChapters({ ...params })
+
   const overlayContent: WireframeContent = {
     [WireframeRegion.VIDEO]: <RotatingCube />,
     [WireframeRegion.MARQUEE]: (
@@ -26,6 +32,9 @@ const YoutubeOverlay = (): React.JSX.Element => {
     [WireframeRegion.SIDEBAR_BOTTOM]: <SplayAnimation />,
     [WireframeRegion.FOOTER_RIGHT]: <YoutubeMarquee />,
   }
+
+  // if (isLoading) return <div>Loading...</div>
+  // if (error) return <div>error...{`${error}`}</div>
 
   return (
     <YoutubeWireframe
---
+++ packages/ui/overlays/src/data/gantt-data.ts
@@ -0,0 +1,29 @@
+import { apiHooks } from "maishatu-fetch-kit"
+import { z } from "zod"
+
+const createChapterSchema = () =>
+  z.object({
+    id: z.string(),
+    title: z.string(),
+    startTime: z.number(),
+    endTime: z.number(),
+    description: z.string(),
+    color: z.string(),
+  })
+
+const chaptersSchema = createChapterSchema()
+
+const ganttSchema = z.object({
+  ...createChapterSchema().shape,
+  subChapters: z.array(chaptersSchema),
+})
+
+const ganttSchemaArray = z.array(ganttSchema)
+
+const fileId = "1CN87_6FFZhSS3jYkJ8KobSaXDuUYzCICPRa79lPdc7E"
+const url = new URL(`http://nixos.local:3000/get_gantt/${fileId}`)
+export const useGanttChapters = apiHooks.createQueryHook(
+  url,
+  ganttSchemaArray,
+  "GET"
+)
