58c0d0de0f00aacf193c4deb504321b9e3d2b137
Paul Gathondu <aulgondu@gmail.com>
2025-09-13 13:39:47 -0700

    feat: deprecate package

--- packages/maishatu-tw-eslint/src/rules/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Linter } from "@typescript-eslint/utils/ts-eslint"
-
-import classNameOrder from "./enforce-order"
-
-const rules = {
-  "classname-order": classNameOrder,
-} satisfies Linter.PluginRules
-
-export = rules
--- packages/maishatu-tw-eslint/src/util/create-rule.ts
+++
@@ -1,7 +0,0 @@
-import { ESLintUtils } from "@typescript-eslint/utils"
-
-import type { ESLintPluginDocs } from "../../rules"
-
-export const createRule = ESLintUtils.RuleCreator<ESLintPluginDocs>(
-  (name) => `https://typescript-eslint.io/rules/${name}`
-)
