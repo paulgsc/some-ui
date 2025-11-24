f68a5d11c42dae7fb653650d74298cbcbccc5cc9
Paul Gathondu <aulgondu@gmail.com>
2025-11-23 23:34:56 -0800

    chore: add browser global

--- packages/eslint/src/configs/base.config.ts
+++ packages/eslint/src/configs/base.config.ts
@@ -40,6 +40,7 @@ export default <Array<ConfigWithExtends>>[
         ...globals.es2020,
         ...globals.node,
         ...globals.browser,
+        browser: "readonly",
       },
     },
     rules: {
