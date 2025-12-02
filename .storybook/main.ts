0735fb62383e0312b2278b90e494630863f73530
Paul Gathondu <aulgondu@gmail.com>
2025-12-01 19:41:29 -0800

    chore: update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,12 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
+import { fileURLToPath } from "node:url";
+import { createRequire } from "node:module";
 import { dirname, join, resolve } from "path"
 import type { StorybookConfig } from "@storybook/react-vite"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const require = createRequire(import.meta.url);
 
 /**
  * This function is used to resolve the absolute path of a package.
