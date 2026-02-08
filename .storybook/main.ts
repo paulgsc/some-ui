f7b0d58ff475245c6053871b6f36dc9b4b271b31
Paul Gathondu <aulgondu@gmail.com>
2026-02-07 19:16:58 -0800

    ci: test build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -50,7 +50,7 @@ function storyGlobs(): Array<string> {
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 
-const someContentPublic = resolve(__dirname, "../packages/some-content/public")
+const someContentPublic = resolve(__dirname, "./packages/some-content/public")
 
 const config: StorybookConfig = {
   stories: storyGlobs(),
