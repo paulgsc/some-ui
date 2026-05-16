c1db7395269ec39d649c39f141be0783d816cfb7
Paul Gathondu <aulgondu@gmail.com>
2026-05-16 13:01:37 -0700

    chore: config

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
       "@slideshow": path.resolve(__dirname, "./packages/ui/slideshow/src"),
       "@emoji": path.resolve(__dirname, "./packages/ui/emoji-animations/src"),
       "@wireframes": path.resolve(__dirname, "./packages/ui/wireframes/src"),
-      "@portfolio-chart": path.resolve(
+      "@portfolio": path.resolve(
         __dirname,
         "./packages/ui/portfolio-chart/src"
       ),
