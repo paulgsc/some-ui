29d499d6920be43ab80e83131065d48c313ee826
Paul Gathondu <aulgondu@gmail.com>
2026-07-25 18:26:44 -0700

    fix: ignore public dir for eslint

--- packages/eslint/src/configs/base.config.ts
+++ packages/eslint/src/configs/base.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(
       "**/dist/**",
       "**/package.json",
       "**/package-lock.json",
+      "**/public/**",
       "**/fixtures/**",
       "**/coverage/**",
       "**/__snapshots__/**",
